004ff79e53522945c13e6bafec93f53634bef962
John Deibert <johndeib121@gmail.com>
2024-12-02 16:45:14 -0500

    classifier w/ imageTesting

--- imageTest.py
+++ imageTest.py
@@ -4,7 +4,7 @@ response = ollama.chat(
     model='llama3.2-vision',
     messages=[{
         'role': 'user',
-        'content': 'What is in this image?',
+        'content': 'Can you tell me if this image supports abortion or not.',
         'images': ['data/data-20241202T145651Z-001/data/images/abortion/1018237459270914049.jpg']
     }]
 )
---
+++ testingTweets.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+
+train_df = pd.read_csv('data/data-20241202T145651Z-001/data/abortion_train.csv')
+dev_df = pd.read_csv('data/data-20241202T145651Z-001/data/abortion_dev.csv')
+
+vectorizer = TfidfVectorizer(max_features=6000)
+
+#fit the vectorizer on the training data and transform both training and dev data
+X_train = vectorizer.fit_transform(train_df['tweet_text'])
+X_dev = vectorizer.transform(dev_df['tweet_text'])
+
+# #feature matrix shape
+# print(X_train.shape)
+
+clf = LogisticRegression()
+
+#train the classifier on the training data
+clf.fit(X_train, train_df['stance'])
+
+#check the accuracy on the training data
+train_accuracy = clf.score(X_train, train_df['stance'])
+print(f'Training Accuracy: {train_accuracy:.4f}')
+
+#make predictions on the dev data
+predictions = clf.predict(X_dev)
+
+#accuracy on the dev data
+dev_accuracy = clf.score(X_dev, dev_df['stance'])
+print(f'Dev Accuracy: {dev_accuracy:.4f}')
+print(classification_report(dev_df['stance'], predictions))
