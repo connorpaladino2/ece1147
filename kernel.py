21fff3648e53b90ff2d76847f6b72b7ddb560f83
connorpaladino2 <connor.p@kredservices.com>
2024-12-03 18:18:58 -0500

    gun control test data

--- kernel.py
+++ kernel.py
@@ -37,6 +37,42 @@ def do_gun_tweet_train():
     output_df = pd.DataFrame(output_arr)
     output_df.to_csv("Output_Text_GunControl.csv")
 
+
+def do_gun_tweet_test():
+    input_df = pd.read_csv("data/data-20241202T145651Z-001/data/gun_control_dev.csv")
+
+    j = 0
+    output_arr = []
+
+    for _, row in input_df.iterrows():
+        arr = []
+        text = row.iloc[2]
+        text = ''.join(char for char in text if 32 <= ord(char) <= 126)
+        j += 1
+        print(f"Row {j}: \"{text}\"")
+
+        output = run_text(f"{row.iloc[2]}")
+
+        i = 0
+        for line in output.strip().split("\n"):
+            if "Question" in line:  # Look for lines containing "Question"
+                try:
+                    answer = float(line.split(":")[1].strip())
+                    arr.append(answer)
+                except (IndexError, ValueError):
+                    print(f"Skipping malformed line: {line}")
+                i += 1
+
+        print(arr)
+        output_arr.append(arr)
+
+    print(output_arr)
+
+    output_df = pd.DataFrame(output_arr)
+    output_df.to_csv("Output_Text_GunControl_Test.csv")
+
+
+
 def do_abortion_tweet_train():
     input_df = pd.read_csv("data/data-20241202T145651Z-001/data/abortion_train.csv")
 
@@ -119,5 +155,6 @@ def do_gun_images():
 
 
 #do_gun_tweet_train()
+do_gun_tweet_test()
 #do_abortion_tweet_train()
-do_gun_images()
+#do_gun_images()
