de66ee89db76e8facd9ab3fe06e02eb01ce2db09
John <JHD36@pitt.edu>
2024-12-03 20:39:10 -0500

    Creating SVM Classifier

--- SVM.py
+++ SVM.py
@@ -7,5 +7,17 @@ import pandas as pd
 def run_SVM(file_path):
     df = pd.read_csv(file_path)
 
-    X = df.iloc[:, :12]
-    y = df.iloc[:, 12]
+    X = df.iloc[1:, :12]
+    y = df.iloc[1:, 12]
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    
+    svm = SVC(kernel='sigmoid', decision_function_shape='ovo')
+    svm.fit(X_train, y_train)
+    y_pred = svm.predict(X_test)
+    
+    # print(accuracy_score(y_test, y_pred))
+    
+    return accuracy_score(y_test, y_pred)
+    
+# run_SVM("gun_train_data.csv")
